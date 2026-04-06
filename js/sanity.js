9a7aec67e7157544e865508dcd0e3900f42fc64f
karanjeet0731 <karanjeets358@gmail.com>
2026-04-07 00:07:23 +0530

    Update sanity.js

--- js/sanity.js
+++ js/sanity.js
@@ -1,1 +1,13 @@
+const client = {
+  projectId: "ncyeom8c",
+  dataset: "production"
+}
 
+async function getPosts() {
+  const query = encodeURIComponent('*[_type == "post"]{title, slug, content}')
+  const url = `https://${client.projectId}.api.sanity.io/v2024-01-01/data/query/${client.dataset}?query=${query}`
+
+  const res = await fetch(url)
+  const data = await res.json()
+  return data.result
+}
