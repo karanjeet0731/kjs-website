a4d5d56ce2d60a6be5e4b7cee7e47f0eb8a0bd72
karanjeet0731 <karanjeets358@gmail.com>
2026-04-07 21:59:48 +0530

    Update sanity.js

--- js/sanity.js
+++ js/sanity.js
@@ -1,36 +1,23 @@
 const client = {
-  projectId: "ncyeom8c",
+  projectId: "nceyom8c",
   dataset: "production"
 }
 
 async function getPosts() {
-  const query = encodeURIComponent('*[_type == "post"]{title, content}')
+  const query = encodeURIComponent(`*[_type == "post"]{
+    title,
+    slug,
+    content
+  }`)
+
   const url = `https://${client.projectId}.api.sanity.io/v2024-01-01/data/query/${client.dataset}?query=${query}`
 
-  const res = await fetch(url)
-  const data = await res.json()
-  return data.result
-}
-
-getPosts().then(posts => {
-  console.log("POSTS:", posts)
-
-  const container = document.getElementById("blog-container")
-
-  if (!posts.length) {
-    container.innerHTML = "<p>No posts found 😢</p>"
-    return
+  try {
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data) // 👈 DEBUG
+    return data.result
+  } catch (err) {
+    console.error("Error:", err)
   }
-
-  posts.forEach(post => {
-    const div = document.createElement("div")
-
-    div.innerHTML = `
-      <h2>${post.title}</h2>
-      <p>${post.content}</p>
-      <hr/>
-    `
-
-    container.appendChild(div)
-  })
-})
+}
